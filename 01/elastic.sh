5a5700ee95a72cdb076af84fdb1a5a95cacfb688
BViki <BViki@users.noreply.github.com>
2025-06-14 07:29:51 +0000

    Make elastic.sh executable and add shebang

--- 01/elastic.sh
+++ 01/elastic.sh
@@ -1,8 +1,9 @@
-docker run -it \
-    --rm \
-    --name elasticsearch \
-    -p 9200:9200 \
-    -p 9300:9300 \
-    -e "discovery.type=single-node" \
-    -e "xpack.security.enabled=false" \
-    elasticsearch:8.17.6
+#!/bin/bash
+
+docker run -it --rm \
+  --name elasticsearch \
+  -p 9200:9200 \
+  -p 9300:9300 \
+  -e "discovery.type=single-node" \
+  -e "xpack.security.enabled=false" \
+  elasticsearch:8.17.6
